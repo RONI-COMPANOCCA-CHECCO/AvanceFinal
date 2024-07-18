a9f79fd773ee68f0af13cd5988d2f402666b430c
rcompanocca <ronicompanocca@gmail.com>
2024-07-17 21:19:11 -0500

    arreglamos las rutas configurando URLS.py

--- Proyecto/Aplicacion/urls.py
+++ Proyecto/Aplicacion/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from .views import lista_cursos
 
 urlpatterns = [
-    path('', views.home),
-    path('registrarCurso/', views.registrarCurso),
-    path('edicionCurso/<codigo>', views.edicionCurso),
-    path('editarCurso/', views.editarCurso),
-    path('eliminarCurso/<codigo>', views.eliminarCurso)
+    path('', views.home, name='home'),
+    path('registrarCurso/', views.registrarCurso, name='registrarCurso'),
+    path('edicionCurso/<str:codigo>/', views.edicionCurso, name='edicionCurso'),
+    path('editarCurso/', views.editarCurso, name='editarCurso'),
+    path('eliminarCurso/<str:codigo>/', views.eliminarCurso, name='eliminarCurso'),
+    path('api/cursos/', lista_cursos, name='lista_cursos'),  # API endpoint
 ]
