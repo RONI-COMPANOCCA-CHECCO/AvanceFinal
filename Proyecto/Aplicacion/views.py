98f54cca57e1f71b7e5485a01b2ff14e0ca4fa49
rcompanocca <ronicompanocca@gmail.com>
2024-07-17 21:16:11 -0500

    corregimos la parte de views.py

--- Proyecto/Aplicacion/views.py
+++ Proyecto/Aplicacion/views.py
@@ -1,38 +1,54 @@
 from django.shortcuts import render, redirect
 from .models import Curso
 from django.contrib import messages
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# API endpoint para obtener la lista de cursos
+@api_view(['GET'])
+def lista_cursos(request):
+    cursos = Curso.objects.all()
+    data = [{"id": curso.id, "codigo": curso.codigo, "nombre": curso.nombre, "creditos": curso.creditos} for curso in cursos]
+    return Response(data)
+
+# Vista para la página principal que lista los cursos
 def home(request):
     cursosListados = Curso.objects.all()
     return render(request, "cursos.html", {"cursos": cursosListados})
 
+# Vista para registrar un nuevo curso
 def registrarCurso(request):
-    codigo=request.POST['txtCodigo']
-    nombre=request.POST['txtNombre']
-    creditos=request.POST['numCreditos']
+    if request.method == "POST":
+        codigo = request.POST['txtCodigo']
+        nombre = request.POST['txtNombre']
+        creditos = request.POST['numCreditos']
 
-    curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+        curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+        messages.success(request, 'Curso registrado exitosamente!')
     return redirect('/')
 
+# Vista para la edición de un curso existente
 def edicionCurso(request, codigo):
     curso = Curso.objects.get(codigo=codigo)
     return render(request, "edicionCurso.html", {"curso": curso})
 
+# Vista para actualizar los datos de un curso existente
 def editarCurso(request):
-    codigo=request.POST['txtCodigo']
-    nombre=request.POST['txtNombre']
-    creditos=request.POST['numCreditos']
+    if request.method == "POST":
+        codigo = request.POST['txtCodigo']
+        nombre = request.POST['txtNombre']
+        creditos = request.POST['numCreditos']
 
-    curso = Curso.objects.get(codigo=codigo)
-    curso.nombre = nombre
-    curso.creditos = creditos
-    curso.save()
-
+        curso = Curso.objects.get(codigo=codigo)
+        curso.nombre = nombre
+        curso.creditos = creditos
+        curso.save()
+        messages.success(request, 'Curso actualizado exitosamente!')
     return redirect('/')
 
+# Vista para eliminar un curso existente
 def eliminarCurso(request, codigo):
-    curso =Curso.objects.get(codigo=codigo)
+    curso = Curso.objects.get(codigo=codigo)
     curso.delete()
-
+    messages.success(request, 'Curso eliminado exitosamente!')
     return redirect('/')
